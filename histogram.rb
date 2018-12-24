0f6cfafdd4511a25bfcf293d429131a540db9e61
Elli <ellimoty@gmail.com>
2018-12-25 00:31:28 +1100

    array and  object in Ruby

---
+++ histogram.rb
@@ -0,0 +1,13 @@
+puts "Enter a phrase you'd like to analyze: "
+text = gets.chomp
+
+words = text.split(" ") # split() method
+
+frequencies = Hash.new(0) # Hash is like object in Js
+
+words.each { |word| frequencies[word] += 1 }
+
+frequencies = frequencies.sort_by { |x, y| y } # sort_by => sorting items from smallest to biggest
+frequencies.reverse!
+
+frequencies.each { |name, count| puts name + " " + count.to_s } # to_s => converting to string
